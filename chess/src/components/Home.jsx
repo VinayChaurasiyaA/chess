87f8d149ddf7be2895a9b7367563a39ceedeba15
VINAY CHAURASIYA <vinaychaurasiya73@gmail.com>
2023-05-11 11:54:46 +0530

    second commit , home and future impletementation

--- chess/src/components/Home.jsx
+++ chess/src/components/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import {AiFillCopy} from 'react-icons/ai'
 
+import '../styles/home.css';
 const Home = () => {
+  const navigate = useNavigate();
+  const handleJoin = () => {
+    navigate('/room/:id')
+    console.log('Joining the room')
+  }
   return (
-    <div>
-      Home
+    <>
+      <h1 className='heading'>Chess.io</h1>
+    <div className='container-home'>
+      <div className = "up">
+        <AiFillCopy className='copy-icon' size={"16px"}/>
+        <input type="text" value={"fetch and show the socket link"} />
+      </div>
+      <div className='down'>
+        <button className='btn-join' onClick={handleJoin}>
+          Join the room
+        </button>
+      </div>
     </div>
+    </>
   )
 }
 
