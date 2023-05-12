ab613e9a1950fba79e1b7df208252720d62c155e
VINAY CHAURASIYA <vinaychaurasiya73@gmail.com>
2023-05-12 23:27:45 +0530

    hanle invite

--- chess/src/components/Home.jsx
+++ chess/src/components/Home.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import {AiFillCopy} from 'react-icons/ai'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AiFillCopy } from "react-icons/ai";
 
-import '../styles/home.css';
+import "../styles/home.css";
 const Home = () => {
+  const [invite, setInvite] = useState(false);
   const navigate = useNavigate();
   const handleJoin = () => {
-    navigate('/room/:id')
-    console.log('Joining the room')
-  }
+    navigate("/room/:id");
+    console.log("Joining the room");
+  };
+  const handleInvite = (e) => {
+    e.preventDefault();
+    // todo implement the invite functionality
+    setInvite(true);
+  };
   return (
     <>
-      <h1 className='heading'>Chess.io</h1>
-    <div className='container-home'>
-      <div className = "up">
-        <AiFillCopy className='copy-icon' size={"16px"}/>
-        <input type="text" value={"fetch and show the socket link"} />
+      <h1 className="heading">Chess.io</h1>
+      <div className="container-home">
+        <div className="up">
+          {invite ? (
+            <>
+              <AiFillCopy className="copy-icon" size={"16px"} />
+              <input type="text" value={"fetch and show the socket link"} />
+            </>
+          ) : (
+            <button
+              className="btn-invite btn-join"
+              onClick={(e) => handleInvite(e)}
+            >
+              invite
+            </button>
+          )}
+        </div>
+        <div className="down">
+          <button className="btn-join" onClick={handleJoin}>
+            Join the room
+          </button>
+        </div>
       </div>
-      <div className='down'>
-        <button className='btn-join' onClick={handleJoin}>
-          Join the room
-        </button>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
